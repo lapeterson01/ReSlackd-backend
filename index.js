1ee3f229ed4a72277eaef36df30f251458abd12c
Logan Peterson <lapeterson01@gmail.com>
2018-03-17 00:26:22 -0400

    Finish passport

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
 const express = require('express');
-const mysql = require('mysql');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const socket = require('socket.io');
-const credentials = require('./sql/sql-credentials');
 const keys = require('./config/dev');
-// require('./models/User')
 require('./services/passport');
 
-const pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'localhost',
-    user            : credentials.username,
-    password        : credentials.password,
-    database        : 'reslackd'
-})
-
 const app = express();
-
-// const sql = mysql.createConnection({
-//     host: "localhost",
-//     user: credentials.username,
-//     password: credentials.password
-// });
-
-// const time = new Date();
-
-// pool.query(`INSERT INTO users (name, imageURL, googleID, createdAt, lastActiveAt, lastLoginAt) VALUES ('Logan', 'example.jpg', '123456', ${time.getTime()}, ${time.getTime()}, ${time.getTime()})`, (err) => {
-//     if (err) throw err;
-//     console.log("values added");
-// })
 
 app.use(bodyParser.json());
 app.use(
--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/dev');
+const sql = require('../db/pool');
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    console.log(id);
     done(null, id);
 })
 
@@ -19,8 +19,18 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            
-            done(null, profile);
+            sql.query('SELECT * FROM users WHERE googleID = ?', [profile.id], (err, existingUser, fields) => {
+                if (err) throw err;
+                if (existingUser.length > 0) {
+                    done(null, profile);
+                } else {
+                    let time = new Date();
+                    sql.query('INSERT INTO users (name, imageURL, googleID, createdAt, lastActiveAt, lastLoginAt) VALUES (?, ?, ?, ?, ?, ?)', [profile.displayName, profile.photos[0].value, profile.id, time.getTime(), time.getTime(), time.getTime()], (err, result) => {
+                        if (err) throw err;
+                        done(null, profile)
+                    });
+                }
+            })
         }
     )
 );
