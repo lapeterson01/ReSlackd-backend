e67cb4f4aa6bcb5a3567d4a40b05acd494dc17f3
Megan Honig <meganhonig@gmail.com>
2018-03-19 10:43:44 -0400

    add dummyuser to require-login middleware

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -3,14 +3,19 @@ const pool = require('../db/pool');
 
 module.exports = app => {
   app.get('/api/channels', requireLogin, async (req, res) => {
-    if (!req.user) return res.status(401).end()
+    //provide list of channels
+    //allow a search in querystring
+    let uID = req.user.uID;
+
+    pool.query('SELECT name FROM channels')
     console.log('channels response')
   });
 
   app.get('/api/channels/:channelId', requireLogin, async (req, res) => {
     console.log('show all messages in channel (can be channel or dm)')
-    // show 20 by default.
-    const message = [{username, userId, text, timestamp, image}]
+    // show 20 by default. allow requests for previous pages
+
+    const message = [{name, userId, text, timestamp, image}]
   });
 
 
@@ -22,13 +27,13 @@ module.exports = app => {
   });
   
   app.get('/api/user/channels', requireLogin, async (req, res) => {
-    if (!req.user) return res.status(401).end()
+    
     console.log('Search for users');
 
   });
 
   app.get('/api/current_user', requireLogin, async (req, res) => {
-    if (!req.user) return res.status(401).end()
+    
     console.log('gives user details for logged in user');
 
   });
