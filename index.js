6a81207fb08f69b06b5c0aa9f3fd7cb5346430a0
Megan Honig <meganhonig@gmail.com>
2018-03-21 11:10:35 -0400

    add cors support

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const socket = require('socket.io');
 const keys = require('./config/keys');
+const cors = require('cors');
 require('./services/passport');
 
 const app = express();
 
+app.use(cors);
 app.use(bodyParser.json());
 app.use(
     cookieSession({
