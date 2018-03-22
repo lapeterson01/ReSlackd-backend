dbbeba62e98fe1310c6de83338f86f329f716725
Megan Honig <meganhonig@gmail.com>
2018-03-22 09:20:07 -0400

    argh

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ require('./routes/authRoutes')(app);
 require('./routes/getRoutes')(app);
 require('./routes/postRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('https://reslackd-frontend.herokuapp.com/client/build'));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.redirect('https://reslackd-frontend.herokuapp.com/');
+    });
+  }
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log('server is running on port 8080')
